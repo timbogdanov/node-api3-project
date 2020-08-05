5978f4adee00de163bcb0c3a0aee376d7e056926
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-08-05 12:55:59 -0700

    set up validate Post

--- server.js
+++ server.js
@@ -43,4 +43,18 @@ function validateUser() {
   };
 }
 
+function validatePost() {
+  return function (req, res, next) {
+    if (!req.body) {
+      res.status(400).json({ message: 'missing post data' });
+    } else if (!req.body.text) {
+      res
+        .status(400)
+        .json({ message: 'missing required text field' });
+    }
+
+    next();
+  };
+}
+
 module.exports = server;
