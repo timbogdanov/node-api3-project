529e3fbc5020afa38c4ba3176f80ba39f793d0b5
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-08-05 15:34:24 -0700

    complete mvp

--- users/userRouter.js
+++ users/userRouter.js
@@ -8,7 +8,7 @@ const router = express.Router();
 const postsRouter = require('../posts/postRouter');
 
 router.post('/', validateUser, (req, res) => {
-  Users.insert(req.user)
+  Users.insert(req.body)
     .then((user) => {
       res.status(200).json(user);
     })
@@ -20,20 +20,25 @@ router.post('/', validateUser, (req, res) => {
     });
 });
 
-router.post('/:id/posts', validateUserId, (req, res) => {
-  const newPost = { ...req.body, user_id: req.params.id };
+router.post(
+  '/:id/posts',
+  validateUserId,
+  validatePost,
+  (req, res) => {
+    const newPost = { ...req.body, user_id: req.headers.id };
 
-  Posts.insert(newPost)
-    .then((post) => {
-      res.status(200).json(post);
-    })
-    .catch((error) => {
-      res.status(500).json({
-        message: 'unable to submit post data',
-        error: error.message,
+    Posts.insert(newPost)
+      .then((post) => {
+        res.status(200).json(post);
+      })
+      .catch((error) => {
+        res.status(500).json({
+          message: 'unable to submit post data',
+          error: error.message,
+        });
       });
-    });
-});
+  }
+);
 
 router.get('/', (req, res) => {
   Users.get()
@@ -77,11 +82,25 @@ router.delete('/:id', validateUserId, (req, res) => {
         .status(201)
         .json({ message: `deleted user id: ${req.params.id}` });
     })
-    .catch();
+    .catch((error) => {
+      res.status(500).json({
+        message: `unable to delete user id: ${req.params.id}`,
+      });
+    });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validateUserId, validateUser, (req, res) => {
+  Users.update(req.params.id, req.body)
+    .then((updatedUser) => {
+      res.status(200).json({
+        message: `updated user with id: ${req.params.id}, records updated: ${updatedUser}`,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: `unable to delete user id: ${req.params.id}`,
+      });
+    });
 });
 
 //custom middleware
@@ -102,7 +121,6 @@ function validateUser(req, res, next) {
   } else if (!req.body) {
     res.status(400).json({ message: 'missing user data' });
   } else {
-    req.user = req.body;
     next();
   }
 }
