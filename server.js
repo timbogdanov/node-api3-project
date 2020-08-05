7843e32d6f9a107fc99d408d862e02afc3a381e5
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-08-05 12:52:26 -0700

    set up validate user middleware

--- server.js
+++ server.js
@@ -19,13 +19,27 @@ function logger(req, res, next) {
 }
 
 function validateUserId(id) {
-  return function validateUserId(req, res, next) {
+  return function (req, res, next) {
     if (req.header.id === id) {
-      req.user = req.header.id;
+      req.user = req.body;
       next();
     } else {
       res.status(400).json({ message: 'invalid user id' });
     }
+  };
+}
+
+function validateUser() {
+  return function (req, res, next) {
+    if (!req.body) {
+      res.status(400).json({ message: 'missing user data' });
+    } else if (!req.body.name) {
+      res
+        .status(400)
+        .json({ message: 'missing required name field' });
+    }
+
+    next();
   };
 }
 
